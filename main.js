edf4191bf3e0381ec9bde8e08a1c146e06d45412
DemetrioPadre <demetriopadre@gmail.com>
2024-01-08 16:27:16 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,24 @@
+// Chiede all utente il suo Nome
+const nome = prompt('Utente inserisci il tuo nome');
+console.log(nome);
+
+//Chiedi all utente il suo cognome
+const cognome = prompt('Utente inserisci il tuo cognome');
+console.log(cognome);
+
+// Chiedi all utente quale colore
+const prefcolor = prompt('Quale è il tuo colore preferito');
+console.log(prefcolor);
+
+
+// password +23
+let password = nome + cognome + prefcolor + '23';
+console.log(password);
+
+//alert
+
+alert('La tua password è  ' + password);
+
+
+
+
